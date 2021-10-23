f3218cdaf1bcf70532be29d267bdb8bad46ae91c
PUSHkin42342443 <alexandderdudarev@gmail.com>
2021-10-23 10:48:20 +0500

    выполнено задание 1, но оно не работатет, почему то, я добавил класс програм, но всё равно не работает

---
+++ Задание 3/Задание 3/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Задание_3
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите занчение х");
+            var x = double.Parse(Console.ReadLine());
+            double y = 2 / A(x) + Math.Cos(x);
+            Console.WriteLine(y);
+            double z = y / B(x);
+            Console.WriteLine(z);
+        }
+        static double A(double x)
+        {
+            return (x * x + 25);
+        }
+        static double B(double x)
+        {
+            return Math.Sqrt(x * x * x * x + 1) + (Math.Cos(x) + Nath.Sin(x)) / 2;
+        }
+    }
+
+}
